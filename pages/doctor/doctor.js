9e476efdfe95b397d4657b2ab9e83fa93b84ae42
Zimomo <lzh120908717@gmail.com>
2020-09-14 22:36:19 +0800

    医生列表卡片
    医生列表卡片

--- pages/doctor/doctor.js
+++ pages/doctor/doctor.js
@@ -1,33 +1,9 @@
 // pages/doctor/doctor.js
-import area from "./area"
-
 Page({
   data: {
-    department: '',
-    search: '',
-    sortOption: [
-      { text: '默认排序', value: 0 },
-      { text: '星级评分', value: 1 },
-      { text: '回答次数', value: 2 },
-      { text: '价格从低到高', value: 3 },
-      { text: '价格从高到低', value: 4 },
-    ],
-    sort: 0,
-    areaList: area,
-    areaName: '全国',
-    areaCode: '100100'
+    doctor_id: ''
   },
   onLoad(options) {
-    this.setData({ department: options.department})
-  },
-  areaConfirm( {detail: { values }} ) {   //对象解构
-    this.selectComponent('#item').toggle();
-    this.setData({ 
-      areaCode: values[1].code,
-      areaName: values[1].name
-    })
-  },
-  areaCancel() {
-    this.selectComponent('#item').toggle();
+    this.setData({ doctor_id: options.doctor_id})
   },
 })
---
+++ pages/doctorList/doctorList.js
@@ -0,0 +1,55 @@
+// pages/doctorList/doctorList.js
+import area from "./area"
+
+Page({
+  data: {
+    department: '',
+    search: '',
+    sortOption: [
+      { text: '默认排序', value: 0 },
+      { text: '星级评分', value: 1 },
+      { text: '回答次数', value: 2 },
+      { text: '价格从低到高', value: 3 },
+      { text: '价格从高到低', value: 4 },
+    ],
+    sort: 0,
+    areaList: area,
+    areaName: '全国',
+    areaCode: '100100',
+    doctors: [
+      { 
+        id: '1',
+        name: '钟南山',
+        imageUrl: '/picture/doctor1.jpg',
+        textPrice: 10.00,
+        telPrice: 13.00
+      },
+      { 
+        id: '2',
+        name: '李文亮',
+        imageUrl: '/picture/doctor2.jpg',
+        textPrice: 10.00,
+        telPrice: 13.00
+      }
+    ]
+  },
+  onLoad(options) {
+    this.setData({ department: options.department})
+  },
+  areaConfirm( {detail: { values }} ) {   //对象解构
+    this.selectComponent('#item').toggle();
+    this.setData({ 
+      areaCode: values[1].code,
+      areaName: values[1].name
+    })
+  },
+  areaCancel() {
+    this.selectComponent('#item').toggle();
+  },
+  jump(event){
+    console.log(event)
+    wx.navigateTo({
+      url: '/pages/doctor/doctor?doctor_id=' + event.currentTarget.id
+    })
+  }
+})
