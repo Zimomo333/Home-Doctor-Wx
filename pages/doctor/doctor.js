f4f2bd9d045c70ba68e1f3c43b56d59d0ba28e0a
Zimomo <lzh120908717@gmail.com>
2020-10-02 21:20:05 +0800

    医生详情页 评论懒加载接口
    医生详情页 评论懒加载接口

--- pages/doctor/doctor.js
+++ pages/doctor/doctor.js
@@ -2,6 +2,7 @@ import myRequest from '../../utils/request'
 // pages/doctor/doctor.js
 Page({
   data: {
+    page: 1,
     doctor_id: '',
     doctorInfo: {},
     comments: []
@@ -9,10 +10,25 @@ Page({
   onLoad(options) {
     this.setData({ doctor_id: options.doctor_id})
     myRequest('/wx_user/get_doctor_message',{id:options.doctor_id},'GET').then((data)=>{
+      data.doctor.goodcommonts = (data.doctor.goodcommonts/data.doctor.allcommonts*5).toFixed(2);
       this.setData({
-        doctorInfo: data.doctor,
-        comments: data.commonts
+        doctorInfo: data.doctor
       })
+    })
+    myRequest('/wx_user/get_comments',{id:options.doctor_id,page:this.data.page},'GET').then((data)=>{
+      this.setData({
+        comments: this.data.comments.concat(data)
+      })
+    })
+  },
+  onReachBottom(){    //上拉懒加载
+    myRequest('/wx_user/get_comments',{id:this.data.doctor_id,page:this.data.page+1},'GET').then((data)=>{
+      if(data.length != 0){   // 判断是否已经没数据了
+        this.setData({
+          page:this.data.page+1,
+          comments: this.data.comments.concat(data)
+        })
+      }
     })
   },
   onClickText() {
