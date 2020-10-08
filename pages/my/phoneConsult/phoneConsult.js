e0c5cc04a0228846ce5393884a03e37c194ccd44
Zimomo <lzh120908717@gmail.com>
2020-10-08 20:18:09 +0800

    电话订单列表 接口调试
    电话订单列表 接口调试

--- pages/my/phoneConsult/phoneConsult.js
+++ pages/my/phoneConsult/phoneConsult.js
@@ -1,30 +1,33 @@
+import myRequest from '../../../utils/request'
+
 Page({
   data: {
     active: 0,
-    ingList: [
-      {
-        id: '123',
-        doctor_name: '钟南山',
-        date: '2020-09-09',
-        price: 15
-      }
-    ],
-    waitCommentList: [
-      {
-        id: '123',
-        doctor_name: '钟南山',
-        date: '2020-09-09',
-        price: 15
-      }
-    ],
-    finishList: [
-      {
-        id: '123',
-        doctor_name: '钟南山',
-        date: '2020-09-09',
-        price: 15
-      }
-    ],
+    ingList: [],
+    waitCommentList: [],
+    finishList: []
+  },
+  onLoad(option){
+    if(option.active!==undefined){
+      this.setData({
+        active: Number(option.active)
+      })
+    }
+    myRequest('/wx_user/find_going_porder',null,'GET').then((data)=>{
+      this.setData({
+        ingList: data
+      })
+    })
+    myRequest('/wx_user/find_evaluate_porder',null,'GET').then((data)=>{
+      this.setData({
+        waitCommentList: data
+      })
+    })
+    myRequest('/wx_user/find_complete_porder',null,'GET').then((data)=>{
+      this.setData({
+        finishList: data
+      })
+    })
   },
   onClickButton(){
     console.log('i am button')
